01ea32e6f8c45857f1e9889ff8345eaaa1f27de5
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-08-16 00:17:39 +0500

    patch: fixed the navbar link (again, it didn't work before)

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,9 @@ export default function Navbar() {
         <header>
             <nav className="w-full flex p-5 justify-between items-center">
                 <Link href="/" className="md:text-3xl text-2xl font-medium">fbisev7</Link>
-                <Button href="https://github.com/nabeelv7/fbisev7-2025">View Code</Button>
+                <a target="_blank" href="https://github.com/nabeelv7/fbisev7-2025">
+                    <Button>View Code</Button>
+                </a>
             </nav>
         </header>
     )
