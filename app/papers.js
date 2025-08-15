ebf45fbb3b09ee6b738a44c6b114fc939dffeee8
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-08-15 21:55:32 +0500

    init

---
+++ app/papers.js
@@ -0,0 +1,84 @@
+// @ts-check
+/**
+ * @typedef {Object} Paper
+ * @property {string} name
+ * @property {string} link
+ */
+
+/**
+ * @type {{ [grade: number]: Paper[] }}
+ */
+const papers = {
+    9: [
+        {
+            name: "Biology",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-I/Final%20Assessment%20Framework%20Model%20Question%20Paper%20Biology%20SSC-I.pdf"
+        },
+        {
+            name: "Chemistry",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-I/Final%20Assessment%20Framework%20Model%20Question%20Paper%20Chemistry%20SSC-I.pdf"
+        },
+        {
+            name: "Computer",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-I/Final%20Assessment%20Framework%20Model%20Question%20Paper%20Chemistry%20SSC-I.pdf"
+        },
+        {
+            name: "English",
+            link: "https://fbise.edu.pk/ModelPaper/2024New/english/Final_Assessment_Framework_Model_Question_Paper_English_SSC-I_updated.pdf"
+        },
+        {
+            name: "Mathematics",
+            link: "https://fbise.edu.pk/ModelPaper/2024New/maths/Final_Assessment_Framework_Model_Question_Paper_Mathematics_SSC-I.pdf"
+        },
+        {
+            name: "Physics",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-I/Final%20Assessment%20Framework%20Model%20Question%20Paper%20Physics%20SSC-I.pdf"
+        },
+        {
+            name: "Urdu",
+            link: "https://fbise.edu.pk/ModelPaper/2024New/urdu/Final_Assessment_Framework_Model_Question_Paper_Urdu_SSC-I.pdf"
+        },
+        {
+            name: "Islamiat",
+            link: "https://fbise.edu.pk/ModelPaper/2024New/islamiyat/Final%20Assessemnt%20Framework%20+%20Model%20Question%20Paper%20SSC-I.pdf"
+        },
+        {
+            name: "Tarjuma",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-I/Final%20Assessment%20Framework%20Tarjuma%20Quran%20+%20Model%20Question%20Paper%20SSC-I.pdf",
+        }
+    ],
+    10: [
+        {
+            name: "Biology",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Biology%20SSC-II.pdf"
+        },
+        {
+            name: "Chemistry",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Chemistry%20SSC-II.pdf"
+        },
+        {
+            name: "Computer",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Computer%20Science%20SSC-II.pdf"
+        },
+        {
+            name: "English",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20English%20SSC-II.pdf"
+        },
+        {
+            name: "Mathematics",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Mathematics%20SSC-II.pdf"
+        },
+        {
+            name: "Pak Studies",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Pakistan%20Studies%20SSC-II.pdf"
+        },
+        {
+            name: "Physics",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Physics%20SSC-II.pdf"
+        },
+        {
+            name: "Urdu",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Urdu%20SSC-II.pdf"
+        },
+    ]
+}
