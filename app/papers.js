22c7e80af2f366156cb0637eb9d917efd012198d
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-08-15 23:04:56 +0500

    added links

--- app/papers.js
+++ app/papers.js
@@ -80,5 +80,77 @@ const papers = {
             name: "Urdu",
             link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/SSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Urdu%20SSC-II.pdf"
         },
+    ],
+    11: [
+        {
+            name: "Biology",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-I/Final%20Assessment%20Framework%20Model%20Question%20Paper%20Biology%20HSSC-I.pdf"
+        },
+        {
+            name: "Chemistry",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks//HSSC-I/Final%20Assessment%20Framework%20Model%20Question%20Paper%20Chemistry%20HSSC-I_.pdf"
+        },
+        {
+            name: "Computer",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks//HSSC-I/Final%20Assessment%20Framework%20Model%20Question%20Paper%20Computer%20Science%20HSSC-I.pdf"
+        },
+        {
+            name: "English",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks//HSSC-I/Final%20Assessment%20Framework%20Model%20Question%20Paper%20Computer%20Science%20HSSC-I.pdf"
+        },
+        {
+            name: "Mathematics",
+            link: "https://fbise.edu.pk/ModelPaper/2024New/maths/Final_Assessment_Framework_Model_Question_Paper_Mathematics_HSSC-I_.pdf"
+        },
+        {
+            name: "Physics",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-I/Final%20Assessment%20Framework%20Model%20Question%20Paper%20Physics%20HSSC-I.pdf"
+        },
+        {
+            name: "Urdu",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-I/Final%20Assessment%20Framework%20&%20Model%20Paper%20Question%20Paper%20Urdu%20HSSC-I_.pdf"
+        },
+        {
+            name: "Islamiat",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-I/Final_Assessment%20Framework%20Islamic%20Compulsory%20HSSC-1.pdf"
+        },
+        {
+            name: "Tarjuma",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-I/Final%20Assessment%20Framework%20Tarjuma%20Quran%20HSSC-I.pdf"
+        }
+    ],
+    12: [
+        {
+            name: "Biology",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Biology%20HSSC-II.pdf"
+        },
+        {
+            name: "Chemistry",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Chemistry%20HSSC-II.pdf"
+        },
+        {
+            name: "Computer",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Computer%20Science%20HSSC-II.pdf"
+        },
+        {
+            name: "English",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20English%20HSSC-II.pdf"
+        },
+        {
+            name: "Mathematics",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Mathematics%20HSSC-II.pdf"
+        },
+        {
+            name: "Physics",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Physics%20HSSC-II.pdf"
+        },
+        {
+            name: "Urdu",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Urdu%20HSSC-II.pdf"
+        },
+        {
+            name: "Pak Studies",
+            link: "https://fbise.edu.pk/ModelPaper/2025/Assessment%20Frameworks/HSSC-II/Final%20Assessment%20Framework%20+%20Model%20Question%20Paper%20Pakistan%20Studies%20HSSC-II.pdf"
+        }
     ]
 }
